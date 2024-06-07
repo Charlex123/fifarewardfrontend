204df170e66dfd4555f19585259ff64da55e25a0
Charles Muoka <charlesmuoka1@gmail.com>
2024-06-07 20:09:31 +0100

    another commit

--- src/components/Socket.ts
+++ src/components/Socket.ts
@@ -5,7 +5,7 @@ const API_URL = process.env.NODE_ENV === "production"
   ? process.env.NEXT_PUBLIC_PRODUCTION_API_URL
   : process.env.NEXT_PUBLIC_API_URL;
 
-const socket = io(API_URL, { transports: ['websocket'], withCredentials: true });
+const socket = io(API_URL!, { transports: ['websocket'], withCredentials: true });
 
 socket.on('connect', () => {
   console.log('Connected to Socket.IO server');
